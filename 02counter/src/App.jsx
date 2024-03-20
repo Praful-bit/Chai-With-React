0a6ad1cdbad867ea7700af9166bb5a766032935c
Praful Gahlot <prafulgahlot0896@gmail.com>
2024-03-20 22:38:37 +0530

    tailwind Css and props

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,7 +1,18 @@
 import "./App.css";
 import { useState } from "react";
+import Card from "./Componenets/Card";
 
 function App() {
+  
+  // let myObj ={
+  //   userName: 'Praful',
+  //   age:21
+  // }
+
+  // let newArray = [
+  //   1,2,3
+  // ]
+
   let [Counter, setCounter] = useState(15);
 
   const addValue = () => {
@@ -20,14 +31,28 @@ function App() {
 
   return (
     <>
-      <h1>Chai aur react</h1>
+      <h1 className="flex text-center justify-center bg-green-400 p-4 rounded-lg ">
+        Chai aur react
+      </h1>
       <h2>Counter value: {Counter}</h2>
 
-      <button onClick={addValue}>Add value{Counter}</button>
+      <button
+        onClick={addValue}
+        className="pb-2 mb-3 mt-3 bg-slate-500 text-black rounded-full ring ring-black"
+      >
+        Add value{Counter}
+      </button>
       <br />
       <hr />
-      <button onClick={removeValue}>Remove value{Counter}</button>
-      <p>footer:- {Counter}</p>
+      <button
+        onClick={removeValue}
+        className="pb-2 mb-3 mt-3 bg-slate-300 text-black rounded-full ring ring-black"
+      >
+        Remove value{Counter}
+      </button>
+
+      <Card userName="chaiaurcode" btnText="Click me" />
+      <Card userName="Praful" btnText="Visit me" />
     </>
   );
 }
