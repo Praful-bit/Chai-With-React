b98cf3cbc144089cecf9343edaf230c9204f34c5
Praful Gahlot <prafulgahlot0896@gmail.com>
2024-04-27 19:54:03 +0530

    now things fine

--- Todo-App/src/Context/TodoContext.js
+++ Todo-App/src/Context/TodoContext.js
@@ -1,22 +1,22 @@
 /* eslint-disable no-unused-vars */
-import {createContext,useContext} from 'react'
+import { useContext, createContext } from 'react'
 
 export const TodoContext = createContext({
-    todos:[
-       {
-        id:1,
-        todo:"todo msg",
-        checked:false
-       },
+    todos: [
+        {
+            id: 1,
+            todo: " Todo msg ",
+            checked: false
+        },
     ],
-    addTodo:(todo)=>{},
-    deleteTodo:(id)=>{},
-    updateTodo:(id,todo)=>{},
-    toggleTodo:(id)=>{}
+    addTodo: (todo) => { },
+    deleteTodo: (id) => { },
+    updateTodo: (id, todo) => { },
+    toggleTodo: (id) => { },
 })
 
 export const TodoProvider = TodoContext.Provider
 
-export const useTodo =()=>{
-    useContext(TodoContext)
+export const useTodo = () => {
+    return useContext(TodoContext)
 }
